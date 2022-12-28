ae45f944397c4adf0a506e17fcccaea63e837da2
YKTV8 <97923330+YKTV8@users.noreply.github.com>
2022-12-27 22:21:38 -0800

    12/27
    updated JS, created button to capture the time of the click event. the onclick function locks in the data upon click just once per section disabling the multiple click to override the original time recorded.

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
-const childname = document.querySelectorAll('childname')
-const child = ['ADAM', 'SARAH', 'JACK', 'SAM']
+// const childname = document.querySelectorAll('childname')
+// const child = ['ADAM', 'SARAH', 'JACK', 'SAM']
 
+// $(document).ready(function(){
+//     $("button").click(function(event){
+//       $("span").text(event.timeStamp);
+//     });
+//   });
+  function captureTime(){
+    // Get current time
+    var currentTime = new Date();
+    //Update the text of the button element to show the capture time
+    document.getElementById("time-button").innerHTML = "" + currentTime;
+    //disable the button
+    document.getElementById("time-button").disabled = true;
+  }
+
+  function captureTimeout(){
+    let currentTime = new Date();
+
+    document.getElementById("timeout-button").innerHTML = "" + currentTime;
+    //disable the button
+    document.getElementById("timeout-button").disabled = true;
+  }
